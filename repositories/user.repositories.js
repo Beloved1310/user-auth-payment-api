ec5b620f16ff5c293d08e1af9d1ed703f972376f
Beloved1310 <adeoluwafisayomi@gmail.com>
2024-02-16 17:37:57 +0100

    refactor/remove-unnecessary-data

--- repositories/user.repositories.js
+++ repositories/user.repositories.js
@@ -6,10 +6,7 @@ const userRepository = {
     const user = await User.findOne({ email }).select('-__v ')
     return user
   },
-  async getOneUserData(item) {
-    const user = await User.findOne(item).select('-__v')
-    return user
-  },
+  
   async updateUserData(queryParams, fields) {
     return User.updateOne(fields, {
       $set: queryParams,
