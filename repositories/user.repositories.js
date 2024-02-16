8a0fa01c6e4c12a0b8e01b2eab6f74f18348c9ec
Beloved1310 <adeoluwafisayomi@gmail.com>
2024-02-16 17:39:05 +0100

    refactor/remove-unnecessary-data

--- repositories/user.repositories.js
+++ repositories/user.repositories.js
@@ -1,21 +1,20 @@
-const User = require('../model/user.model');
-
+const User = require('../model/user.model')
 
 const userRepository = {
   async getOneUser(email) {
     const user = await User.findOne({ email }).select('-__v ')
     return user
   },
-  
+
   async updateUserData(queryParams, fields) {
     return User.updateOne(fields, {
       $set: queryParams,
     })
   },
   async createUser(createUser) {
-    const savedUser = await User.create(createUser);
-    const { _id, __v, password, ...data } = savedUser.toObject();
-    return data;
+    const savedUser = await User.create(createUser)
+    const { _id, __v, password, ...data } = savedUser.toObject()
+    return data
   },
 }
 
